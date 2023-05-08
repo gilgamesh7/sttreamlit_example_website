37cbb11b88e1b88ff1859dcacd45670e1204a826
gilgamesh7 <felidaesrule@yahoo.com>
2023-05-08 18:59:32 +1200

    Themes, forms, styles

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 # Links
 - [Youtube video](https://www.youtube.com/watch?v=VqgUkExPvLY)
 - [Emoji codes](https://www.webfx.com/tools/emoji-cheat-sheet/)
+- [JSON Animations](https://lottiefiles.com)
+- [Example forms](https://formsubmit.co)
+- [Steamlit Themes](https://blog.streamlit.io/introducing-theming/)
 
 # Instructions - local
 - python3 -m venv venv --upgrade-deps
--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 import streamlit as st
 from streamlit_lottie import st_lottie
 import requests
+from PIL import Image
 
 st.set_page_config(page_title="My first Streamlit website", page_icon=":tada:", layout="wide")
+
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+local_css("style/style.css")
+
+img_spock = Image.open("images/spock.jpg")
+img_scotty = Image.open("images/scotty.jpg")
 
 def load_lottie_animation(url):
     response = requests.get(url)
@@ -44,3 +55,60 @@ with st.container():
     with right_column:
         coding_animation_url = load_lottie_animation("https://assets3.lottiefiles.com/packages/lf20_4kx2q32n.json")
         st_lottie(coding_animation_url, height=300, key="coding")
+
+    with st.container():
+        st.write("---")
+        st.header("Second section")
+        st.write("##")
+
+        image_column, text_column = st.columns((1,2))
+        with image_column:
+            st.image(img_spock)
+        with text_column:
+            st.subheader("A column for text")
+            st.write(
+                """
+                Now all the other gods and men, lords of chariots, slumbered the whole night through, 
+                but Zeus was not holden of sweet sleep, 
+                for he was pondering in his heart how he might do honour to Achilles 
+                and lay many low beside the ships of the Achaeans. 
+                And this plan seemed to his mind the best, [5] to send to Agamemnon, 
+                son of Atreus, a baneful dream. 
+                """
+            )
+            st.markdown("[Iliad Book 2](http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0134%3Abook%3D2)")
+
+        st.write("##")
+        text_column, image_column = st.columns((1,2))
+        with image_column:
+            st.image(img_scotty)
+        with text_column:
+            st.subheader("A second column for text")
+            st.write(
+                """
+                Now when they were marshalled, the several companies with their captains, the Trojans came on with clamour 
+                and with a cry like birds, even as the clamour of cranes ariseth before the face of heaven, 
+                when they flee from wintry storms and measureless rain, [5] and with clamour fly toward the streams of Ocean, bearing slaughter and death to Pigmy men, and in the early dawn they offer evil battle.
+                """
+            )
+            st.markdown("[Iliad Book 3](http://www.perseus.tufts.edu/hopper/text?doc=Perseus%3Atext%3A1999.01.0134%3Abook%3D3)")
+
+        with st.container():
+            st.write("---")
+            st.write("Contact Form")
+            st.write("##")
+
+            contact_form = """
+                <form action="https://formsubmit.co/felidaesrule@yahoo.com" method="POST">
+                    <input type="text" name="name" placeholder="Your Name" required>
+                    <input type="email" name="email" placeholder="Your Email" required>
+                    <textarea name="message" placeholder="Your message" required></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            """
+
+            left_column, right_column = st.columns(2)
+            with left_column:
+                st.markdown(contact_form, unsafe_allow_html=True)
+            with right_column:
+                st.empty()
